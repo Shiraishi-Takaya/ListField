11e34474f2467b05942d1806274e729234335819
shiraishigumi01 <shiraishigumi01@gmail.com>
2022-03-16 10:11:27 +0900

    add: タスク追加フォームを作成
    indexのTaskComponentにタスクを追加する用のフォームを作成(HTMLのみ)

--- public/js/app.js
+++ public/js/app.js
@@ -19300,8 +19300,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
+
+var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<hr><div><form><input type=\"text\" placeholder=\"タイトルを入力\"><br><input type=\"date\"><div><label><input type=\"radio\" name=\"color\" value=\"0\">0</label><label><input type=\"radio\" name=\"color\" value=\"1\">1</label><label><input type=\"radio\" name=\"color\" value=\"2\">2</label><label><input type=\"radio\" name=\"color\" value=\"3\">3</label><label><input type=\"radio\" name=\"color\" value=\"4\">4</label><label><input type=\"radio\" name=\"color\" value=\"5\">5</label><label><input type=\"radio\" name=\"color\" value=\"6\">6</label><label><input type=\"radio\" name=\"color\" value=\"7\">7</label></div><textarea placeholder=\"詳細を入力\"></textarea></form></div>", 2);
+
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("ul", null, [((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($props.tasks, function (task) {
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("ul", null, [((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($props.tasks, function (task) {
     return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("li", {
       key: task
     }, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(task.title), 1
@@ -19309,7 +19312,9 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
     );
   }), 128
   /* KEYED_FRAGMENT */
-  ))]);
+  ))])]), _hoisted_1], 64
+  /* STABLE_FRAGMENT */
+  );
 }
 
 /***/ }),
--- resources/js/components/TasksComponent.vue
+++ resources/js/components/TasksComponent.vue
@@ -1,9 +1,29 @@
 <template>
-    <ul>
-        <li v-for="task in tasks" :key="task">
-            {{ task.title }}
-        </li>
-    </ul>
+    <div>
+        <ul>
+            <li v-for="task in tasks" :key="task">
+                {{ task.title }}
+            </li>
+        </ul>
+    </div>
+    <hr>
+    <div>
+        <form>
+            <input type="text" placeholder="タイトルを入力"><br>
+            <input type="date">
+            <div>
+                <label><input type="radio" name="color" value="0">0</label>
+                <label><input type="radio" name="color" value="1">1</label>
+                <label><input type="radio" name="color" value="2">2</label>
+                <label><input type="radio" name="color" value="3">3</label>
+                <label><input type="radio" name="color" value="4">4</label>
+                <label><input type="radio" name="color" value="5">5</label>
+                <label><input type="radio" name="color" value="6">6</label>
+                <label><input type="radio" name="color" value="7">7</label>
+            </div>
+            <textarea placeholder="詳細を入力"></textarea>
+        </form>
+    </div>
 </template>
 
 <script>
