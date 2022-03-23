738d5c109839af634467c00ec3bbb987023133ec
shiraishigumi01 <shiraishigumi01@gmail.com>
2022-03-23 10:27:45 +0900

    add: vue-grid-layoutのインストールと試験的な実装
    vue-grid-layout 3.0.0-beta1をインストールしListComponentに試験的に実装

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,13 @@ require('./bootstrap');
 
 import { createApp } from 'vue';
 import ListComponent from './components/ListComponent.vue';
+import VueGridLayout from "vue-grid-layout";
+import { GridItem, GridLayout } from "vue-grid-layout";
 
 createApp({
     components:{
-        ListComponent
+        ListComponent,
+        GridItem,
+        GridLayout,
     }
-}).mount('#app');
+}).use(VueGridLayout).mount('#app');
